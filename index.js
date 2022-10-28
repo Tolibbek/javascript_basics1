9259bf588c4361a6ee0880ce965664ee469d6f68
Tolibbek <tolibbekalisherov876@gmail.com>
2022-10-28 14:11:57 +0500

    first

---
+++ index.js
@@ -0,0 +1,126 @@
+/*      DAT type
+let text = 'Tolibbek' //string 
+let age = 14 //number
+let bool = false // boolean
+let number; //undefined
+console.log(number);
+console.log(h);//null
+
+*/
+
+
+
+/*
+//object
+let Person ={
+    name:"Tolibbek",//string
+    age: 14,//number
+    isMarried:false //boolean
+}
+console.log(Person.name);
+console.log(Person.age);
+console.log(Person.isMarried);
+
+
+*/
+
+
+
+/*
+//arreay
+
+let array =["Acer" , "HP" , "Lenovo" , "Asus"]
+console.log(array);
+array[0]="hp"
+array[1]="acer"
+array[2]="asus"
+array[3]="lenovo"
+console.log(array);
+*/
+
+
+
+/*
+
+let x = 5
+let y=4
+let z=3
+
+console.log(x+y-z);
+console.log(x-y+z);
+console.log(x+y+z);
+console.log(x-y-z);
+console.log(x*y*z);
+console.log(x/y/z);
+console.log(x*y+z);
+console.log(x*y-z);
+console.log(x*y/z);
+console.log(x % y);
+
+
+x++ ICREMENT
+console.log(x);
+
+x-- DECREMENT
+console.log(x);
+
+
+let b=6
+let c=6
+console.log(b==c);
+console.log(6 === "6");
+console.log(4/11);
+console.log("4"-4);
+console.log("4"/4);
+console.log("4"*4);
+console.log("4"*"4");
+
+
+alert("hello")
+let answer= confirm("What's your name?")
+console.log(answer);
+let answers= prompt("What's your name?")
+console.log(answers);
+*/
+
+
+
+//first way
+
+// for (let i=0 ;i<= 10;i++){
+//     console.log(i);
+// }  chunmadm
+
+
+/*
+//Second way
+
+let i = 0;
+do{
+console.log(i);
+i++ 
+}
+while(
+i<10
+)*/
+/*
+//third way
+let i =0
+while(i<10){
+    console.log(i)
+    i++
+}
+*/
+// let answers =[]
+// let questions =[
+//     'Whats tour name',
+//     'how old are you',
+//     'whATs your number'
+// ]
+
+// for  (let i=0;i<questions.length;i++){
+//     answers[i]=prompt(questions[i])
+// }
+// console.log(answers);
+
+
